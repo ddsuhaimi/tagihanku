ec1bbb925ed6b993837d30d10df401f588e02d24
dediacc <dedisuhaimiacc@gmail.com>
2020-05-11 04:10:26 +0700

    fix redux debugger problem, wont load on mobile otherwise

--- src/store.js
+++ src/store.js
@@ -1,16 +1,20 @@
+
 import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from './reducers'
 import thunk from 'redux-thunk'
 
 const middleware = [thunk]
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose 
+
 const store = createStore(
   rootReducer,
   {},
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(
+    applyMiddleware(...middleware)
   )
 )
 
 export default store
+// debugger
+// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
