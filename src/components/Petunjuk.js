a4091a3fd729bdef211c9bb41e31b1389c84fea9
dediacc <dedisuhaimiacc@gmail.com>
2020-05-12 03:07:58 +0700

    fix tanggal

--- src/components/Petunjuk.js
+++ src/components/Petunjuk.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react'
+import { Modal } from 'react-bootstrap'
+
 
 export class Petunjuk extends Component {
+  state = {
+    showModal: false
+  }
+
+  handleClose = e => {
+    this.setState({showModal: false})
+  }
+  
   render() {
     return (
       <div className="petunjuk py-4">
@@ -14,10 +24,24 @@ export class Petunjuk extends Component {
               <li>Setelah selesai, klik tombol <i className="fas-fa-file-image"></i> untuk melihat hasil</li>
               <li>Jika sudah benar, maka klik tombol download untuk mendowload invoice dalam format <code>jpeg</code> </li>
             </ul>
+              <a className="btn btn-info btn-sm text-white" onClick={() => this.setState({showModal: true})} >Lihat update</a>
+          <Modal show={this.state.showModal} onHide={this.handleClose} size="xl">
+            <Modal.Header closeButton>
+              <Modal.Title>Update</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <span className="font-weight-bold">12/05</span>
+              <ul>
+                <li>Input tanggal hanya dengan klik (<code>DatePicker</code>)</li>
+              </ul>
+            </Modal.Body>
+          </Modal>
           </div>
           </div>
     )
   }
 }
+
+
 
 export default Petunjuk
